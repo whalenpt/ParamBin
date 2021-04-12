a5d4420a087b9593c8d826ff58a0c66a3be926a2
Patrick Whalen <whalenpt@gmail.com>
2021-04-11 18:14:51 -0700

    Testing ParamBin in file test2.cpp.

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -34,56 +34,59 @@ int main(int argc,char* argv[])
   input.set("GROUP1",group1);
 
   ParamBin group2;
+
   group2.set("var1",5.3e8);
-  group2.set("var2","1,2,3");
-
-  ParamBin group3;
-  group3.set("var1",1);
-  group3.set("var2","subsubgroup");
-
-  group2.set("GROUP3",group3);
-  input.set("GROUP2",group2);
-
-  std::vector<std::string> str_vals;
-  str_vals.push_back("first val");
-  str_vals.push_back("second val");
-  str_vals.push_back("third val");
-  input.set("string vector",str_vals);
-
-  std::vector<double> dbl_vals;
-  dbl_vals.push_back(1.0e-4);
-  dbl_vals.push_back(1e4);
-  dbl_vals.push_back(3.14);
-  input.set("double vector",dbl_vals);
-
+//  group2.set("var2","1,2,3");
   std::cout << input << std::endl << std::endl;
-  std::cout << "Clearing var1,var3, and var5..." << std::endl;
-  input.clear("var1");
-  input.clear("var3");
-  input.clear("var5");
-  std::cout << input << std::endl << std::endl;
-
-  std::cout << "Clearing GROUP1..." << std::endl;
-  input.clear("GROUP1");
-  std::cout << input << std::endl << std::endl;
-
-  std::cout << "Clearing GROUP3..." << std::endl;
-  input.getBin("GROUP2").clear("GROUP3");
-  std::cout << input << std::endl << std::endl;
-
-  ParamBin& bin = input.getBin("GROUP2");
-  bin.set("new value",2.4445);
-  group2.set("other new value","WHERE AM I?");
-
-  std::cout << "ADDED A NEW VALUE! " << std::endl;
-  std::cout << input << std::endl << std::endl;
-
-  const ParamBin& const_bin = input.getBin("GROUP2");
-
-  std::cout << "ACCESS A VALUE FROM reference to a constant bin " << std::endl;
-  std::cout << "GROUP2 var2 value = " << const_bin.getStr("var2") << std::endl << std::endl;
-  std::cout << input << std::endl << std::endl;
-
+//
+//  ParamBin group3;
+//  group3.set("var1",1);
+//  group3.set("var2","subsubgroup");
+//
+//  group2.set("GROUP3",group3);
+//  input.set("GROUP2",group2);
+//
+//  std::vector<std::string> str_vals;
+//  str_vals.push_back("first val");
+//  str_vals.push_back("second val");
+//  str_vals.push_back("third val");
+//  input.set("string vector",str_vals);
+//
+//  std::vector<double> dbl_vals;
+//  dbl_vals.push_back(1.0e-4);
+//  dbl_vals.push_back(1e4);
+//  dbl_vals.push_back(3.14);
+//  input.set("double vector",dbl_vals);
+//
+//
+//
+//  std::cout << "Clearing var1,var3, and var5..." << std::endl;
+//  input.clear("var1");
+//  input.clear("var3");
+//  input.clear("var5");
+//  std::cout << input << std::endl << std::endl;
+//
+//  std::cout << "Clearing GROUP1..." << std::endl;
+//  input.clear("GROUP1");
+//  std::cout << input << std::endl << std::endl;
+//
+//  std::cout << "Clearing GROUP3..." << std::endl;
+//  input.getBin("GROUP2").clear("GROUP3");
+//  std::cout << input << std::endl << std::endl;
+//
+//  ParamBin& bin = input.getBin("GROUP2");
+//  bin.set("new value",2.4445);
+//  group2.set("other new value","WHERE AM I?");
+//
+//  std::cout << "ADDED A NEW VALUE! " << std::endl;
+//  std::cout << input << std::endl << std::endl;
+//
+//  const ParamBin& const_bin = input.getBin("GROUP2");
+//
+//  std::cout << "ACCESS A VALUE FROM reference to a constant bin " << std::endl;
+//  std::cout << "GROUP2 var2 value = " << const_bin.getStr("var2") << std::endl << std::endl;
+//  std::cout << input << std::endl << std::endl;
+//
 //  const_bin.set("var3","value");   //This will fail to compile!
 
 
