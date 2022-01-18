13e4180d1ad37131619603146b903ac53ea50653
Patrick Whalen <whalenpt@gmail.com>
2022-01-17 21:38:56 -0800

    ParamBin constructor created for maps

--- include/ParamBin/parambin.hpp
+++ include/ParamBin/parambin.hpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <map>
 #include <string>
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -13,7 +12,7 @@
 #include <iterator>
 #include <exception>
 #include <memory>
-#include "pwstrings.h"
+#include "ParamBin/parambinstrings.h"
 
 std::ifstream& readNextLine(std::ifstream& fin,std::string& line_feed);
 void lineToNameVal(const std::string& line_feed,std::string& name,std::string& vals);
@@ -107,8 +106,9 @@ class ParamBin{
 
     public:
         ParamBin();
-        ~ParamBin(); 
+        ~ParamBin() {}; 
         ParamBin(const ParamBin& bin);
+        ParamBin(const ParamMap& map);
         ParamBin(const char* FILE);
         ParamBin(std::string fileName);
         ParamBin& operator = (const ParamBin& bin);
@@ -116,39 +116,60 @@ class ParamBin{
         void loadParamFile(std::string FILE); 
         void loadParamFile(const char* FILE) {loadParamFile(std::string(FILE));}
   
-        double getDbl(const std::string&) const;
-        int getInt(const std::string&) const;
-        bool getBool(const std::string&) const;
-        bool getBoolF(const std::string&) const;
-        bool getBoolT(const std::string&) const;
-        std::string getStr(const std::string&) const;
-        std::string getStrL(const std::string& nm) const;
-        std::string getStrU(const std::string& nm) const;
-        char getChar(const std::string&) const;
+        inline double getDbl(const std::string& nm) const { return get<double>(nm); }
+        inline float getFloat(const std::string& nm) const { return get<float>(nm); }
+        inline int getInt(const std::string& nm) const {return get<int>(nm);}
+        inline std::string getStr(const std::string& nm) const {return get<std::string>(nm);}
+        inline std::string getStrL(const std::string& nm) const {
+            return pwbin::stringLowerCase(getStr(nm));}
+        inline std::string getStrU(const std::string& nm) const {
+            return pwbin::stringUpperCase(getStr(nm));}
+        inline char getChar(const std::string& nm) const {return get<char>(nm);}
+        inline bool getBool(const std::string& nm) const {
+            std::string val = get<std::string>(nm);
+            return (val == "on" ? true : false);
+        }
+        inline bool getBoolF(const std::string& nm) const {
+            return (inBin(nm) ? getBool(nm) : false); 
+        }
+        inline bool getBoolT(const std::string& nm) const {
+            return (inBin(nm) ? getBool(nm) : true); 
+        }
 
         // get a child bin
         ParamBin& getBin(const std::string& name);
         const ParamBin& getBin(const std::string& name) const;
 
         // get the parent bin
-        ParamBin& parentBin() {return *parent;} 
-        const ParamBin& getParentBin() const {return *parent;}
+        ParamBin& parentBin() {return *m_parent;} 
+        const ParamBin& getParentBin() const {return *m_parent;}
 
         // get map of all params
-        ParamMap getParamMap() const; 
+        ParamMap getParamMap() const {return m_params;} 
 
         // get map of all child bins
-        const BinMap& getChildBins() const {return children;}
+        const BinMap& getChildBins() const {return m_children;}
 
-        std::vector<double> getDblVec(const std::string&) const;
-        std::vector<int> getIntVec(const std::string&) const;
-        std::vector<std::string> getStrVec(const std::string&) const;
+        inline std::vector<double> getDblVec(const std::string& nm) const
+        { return getVec<double>(nm); }
+        inline std::vector<int> getIntVec(const std::string& nm) const
+        { return getVec<int>(nm);}
+        inline std::vector<float> getFloatVec(const std::string& nm) const
+        { return getVec<float>(nm); }
+        inline std::vector<std::string> getStrVec(const std::string& nm) const
+        { return getVec<std::string>(nm); }
   
         template<typename T>
         void get(const std::string& nm,T& val) const;  // get a param of type T 
-  
+
         template<typename T>
-        void get(const std::string& nm,std::vector<T>& val) const;  // get a param of type T 
+        T get(const std::string& nm) const; // get a param using get<T>(name)
+
+        template<typename T>
+        void get(const std::string& nm,std::vector<T>& val) const;  
+
+        template<typename T>
+        std::vector<T> getVec(const std::string& nm) const;  // get a vector using getVec<T>
 
         template<typename T>
         void set(const NamedParam<T>& named_param);
@@ -162,6 +183,7 @@ class ParamBin{
         void set(const std::string&,const ParamBin& bin);
 
         void setBool(const std::string& nm,bool);
+
         void set(const NamedBin& named_bin);
 
         // Bins are deep copied into the tree structure
@@ -187,16 +209,16 @@ class ParamBin{
 
     private:
 
-        ParamBin* parent;
-        ParamMap params; 
-        BinMap children;
-        AliasMap alias_map;
-        AliasMap reverse_alias_map;
+        ParamBin* m_parent;
+        ParamMap m_params; 
+        BinMap m_children;
+        AliasMap m_alias_map;
+        AliasMap m_ralias_map;
 
         std::string setParamKey(const std::string& name);
         std::string getStrParam(const std::string& name) const;
         void printBin(std::ostream& os) const;
-				void scanYAML(std::ifstream& fin);
+        void scanYAML(std::ifstream& fin);
 
         bool searchAliasTree(const std::string& key,std::string& strval) const;
         bool searchParamMap(const std::string& key,std::string& strval) const;
@@ -226,62 +248,76 @@ ParamBin& ParamBin::operator<<(const NamedParam<T>& named_param)
 template<typename T> 
 std::string convertToString(T val) 
 {
+    // Use std::to_string approach to get string representation of value
 	std::string str(std::to_string(val));
-	int offset = 1;
+	// check for decimal number, else use built-in std::to_string
+    if(str.find('.') == std::string::npos)
+        return str;
+   	int offset = 1;
 	if(str.find_last_not_of('0')==str.find('.')){
 		offset = 0;
 	}
 	str.erase(str.find_last_not_of('0') + offset,std::string::npos);
 
 
+    // check for zeros (-> std::to_string does not provide high enough precision)
+    bool to_string_worked = false;
+    for(auto& ch : str){
+        if(ch != '0' && ch != '.' && ch !='-'){
+            to_string_worked = true;
+            break;
+        }
+    }
+
+    // Use stringstream approach to get string representation of value
 	std::ostringstream oss;
+	oss.precision(16);
 	oss << std::scientific << val;
 	std::string stre = oss.str();
 
-	// Check for decimal point
-	if(pw::countCharacters(stre,'.') > 0){
-		//std::cout << "VAL STRING: " << stre << std::endl;
-    // Found decimal point
-		//std::cout << "FOUND DECIMAL POINT: " << valstr << std::endl;
-		std::string decimal_part = pw::subString(stre,'.','e');
-		size_t pos = decimal_part.find_last_not_of('0');
-		if(pos != std::string::npos){
-  			decimal_part = decimal_part.substr(0,pos+1);
-		} else{
-				// 1.00000e5 - > 1.0e5
-  			decimal_part = "0";
-		}
-		std::string exponent_part = pw::stripLast(stre,'e');
-		std::string exp_num,exp_sign;
-		// Remove '+' exponent sign for string storage
-		if(exponent_part.find_first_of('-') != std::string::npos){
-			exp_sign = "-";
-			exp_num = exponent_part.substr(1,std::string::npos);
-		} else if(exponent_part.find_first_of('+') != std::string::npos){
-			exp_sign = "";
-			exp_num = exponent_part.substr(1,std::string::npos);
-		} else{
-			exp_sign = "";
-			exp_num = exponent_part.substr(0,std::string::npos);
-		}
-		pos = exp_num.find_first_not_of('0');
-		if(pos != std::string::npos){
-   		exp_num = exp_num.substr(pos,std::string::npos);
-		} else
-			// replace 1.3e000 with 1.3e0
-			exp_num = "0";
+    // std::to_string did not work on decimal/scientific number -> must use stringstream
+	if(!to_string_worked)
+	    str = stre;
 
-		exponent_part = exp_sign + exp_num;
 
-		std::string whole_part = pw::stripFirst(stre,'.');
-		stre =  whole_part + "." + decimal_part + "e" + exponent_part;
-		// std::cout << "EXPONENTIAL FORM: " << stre << std::endl;
-	}
+    std::string decimal_part = pwbin::subString(stre,'.','e');
+    size_t pos = decimal_part.find_last_not_of('0');
+    if(pos != std::string::npos){
+        decimal_part = decimal_part.substr(0,pos+1);
+    } else{
+            // 1.00000e5 - > 1.0e5
+        decimal_part = "0";
+    }
+    std::string exponent_part = pwbin::stripLast(stre,'e');
+    std::string exp_num,exp_sign;
+    // Remove '+' exponent sign for string storage
+    if(exponent_part.find_first_of('-') != std::string::npos){
+        exp_sign = "-";
+        exp_num = exponent_part.substr(1,std::string::npos);
+    } else if(exponent_part.find_first_of('+') != std::string::npos){
+        exp_sign = "";
+        exp_num = exponent_part.substr(1,std::string::npos);
+    } else{
+        exp_sign = "";
+        exp_num = exponent_part.substr(0,std::string::npos);
+    }
+    pos = exp_num.find_first_not_of('0');
+    if(pos != std::string::npos){
+    exp_num = exp_num.substr(pos,std::string::npos);
+    } else
+        // replace 1.3e000 with 1.3e0
+        exp_num = "0";
+
+    exponent_part = exp_sign + exp_num;
+
+    std::string whole_part = pwbin::stripFirst(stre,'.');
+    stre =  whole_part + "." + decimal_part + "e" + exponent_part;
+    // std::cout << "EXPONENTIAL FORM: " << stre << std::endl;
 	// Check if exponential form is significantly shorter than fixed point
 	int FEWER_SPACES = 0;
 	if( (stre.size()+FEWER_SPACES) < str.size()) 
-			return stre;
-	return str;
+        str = stre;
+    return str;
 }
 
 template<typename T> 
@@ -303,6 +339,9 @@ std::string convertToString(std::string val);
 
 template<> 
 std::string convertToString(const char* val);
+
+template<> 
+std::string convertToString(char val);
 
 template <typename T>
 void convertFromString(const std::string& str,T& val)
@@ -339,7 +378,7 @@ void ParamBin::set(const NamedParam<T>& named_param)
 {
     std::string key = setParamKey(named_param.name());
     std::string strVal = convertToString(named_param.value());
-    params[key] = strVal;
+    m_params[key] = strVal;
 }
 
 template<typename T>
@@ -350,16 +389,32 @@ void ParamBin::get(const std::string& name,T& val) const
 }
 
 template<typename T>
+T ParamBin::get(const std::string& name) const
+{
+    T val;
+    get(name,val);
+    return val;
+}
+
+template<typename T>
 void ParamBin::get(const std::string& name,std::vector<T>& vals) const
 {
     std::string strval = getStrParam(name);
-    std::vector<std::string> strvec = pw::parseString(strval,',');
+    std::vector<std::string> strvec = pwbin::parseString(strval,',');
     vals.clear();
     for(auto strval : strvec){
         T val;
         convertFromString<T>(strval,val);
         vals.push_back(val);
     }
+}
+
+template<typename T>
+std::vector<T> ParamBin::getVec(const std::string& name) const
+{
+    std::vector<T> vals;
+    get(name,vals);
+    return vals;
 }
 
 
--- src/parambin.cpp
+++ src/parambin.cpp
@@ -6,15 +6,14 @@
 #include <map>
 #include <set>
 #include <filesystem>
-
-#include "parambin.hpp"
-#include "pwstrings.h"
+#include "ParamBin/parambin.hpp"
+#include "ParamBin/parambinstrings.h"
 
 template<>
 void ParamBin::get(const std::string& name,double& val) const
 {
     std::string strval = getStrParam(name);
-    std::string rawval = pw::eatWhiteSpace(pw::removeSubString(strval,'[',']'));
+    std::string rawval = pwbin::eatWhiteSpace(pwbin::removeSubString(strval,'[',']'));
     convertFromString<double>(rawval,val);
 }
 
@@ -22,74 +21,67 @@ template<>
 void ParamBin::get(const std::string& name,std::vector<double>& vals) const
 {
     std::string strval = getStrParam(name);
-    std::vector<std::string> strvec = pw::parseString(strval,',');
+    std::vector<std::string> strvec = pwbin::parseString(strval,',');
     vals.clear();
     for(auto strval : strvec){
         double val;
-        std::string rawval = pw::eatWhiteSpace(pw::removeSubString(strval,'[',']'));
+        std::string rawval = pwbin::eatWhiteSpace(pwbin::removeSubString(strval,'[',']'));
         convertFromString<double>(rawval,val);
         vals.push_back(val);
     }
 }
 
 // Only children are copied, parent bin is considered root of the tree
-ParamBin::ParamBin(const ParamBin& bin) : parent(nullptr),
-    params(bin.params),
-    children(),
-    alias_map(bin.alias_map),
-    reverse_alias_map(bin.reverse_alias_map)
+ParamBin::ParamBin(const ParamBin& bin) : m_parent(nullptr),
+    m_params(bin.m_params),
+    m_children(),
+    m_alias_map(bin.m_alias_map),
+    m_ralias_map(bin.m_ralias_map)
 {
-    for(auto it = bin.children.cbegin() ; it != bin.children.cend(); it++)
+    for(auto it = bin.m_children.cbegin() ; it != bin.m_children.cend(); it++)
     {
         std::string bin_name = it->first;
         std::unique_ptr<ParamBin> child_bin(new ParamBin(*it->second));
-        child_bin->parent = this;
-        children[bin_name] = std::move(child_bin);
+        child_bin->m_parent = this;
+        m_children[bin_name] = std::move(child_bin);
     }
+}
+
+ParamBin::ParamBin(const ParamMap& map) : m_parent(nullptr),
+    m_params(map),
+    m_children(),
+    m_alias_map(),
+    m_ralias_map()
+{
 }
 
 // Assignment
 ParamBin& ParamBin::operator = (const ParamBin& bin)
 {
-    parent = nullptr;
-    params = bin.params;
-    alias_map =  bin.alias_map;
-    reverse_alias_map = bin.reverse_alias_map;
+    m_parent = nullptr;
+    m_params = bin.m_params;
+    m_alias_map =  bin.m_alias_map;
+    m_ralias_map = bin.m_ralias_map;
     {
-        for(auto it = bin.children.cbegin() ; it != bin.children.cend(); it++)
+        for(auto it = bin.m_children.cbegin() ; it != bin.m_children.cend(); it++)
         {
             std::string bin_name = it->first;
             std::unique_ptr<ParamBin> child_bin(new ParamBin(*it->second));
-            child_bin->parent = this;
-            children[bin_name] = std::move(child_bin);
+            child_bin->m_parent = this;
+            m_children[bin_name] = std::move(child_bin);
         }
     }
     return *this;
 }
 
 ParamBin::ParamBin() : 
-    parent(nullptr)
-{
-}
+    m_parent(nullptr) {}
 
 ParamBin::ParamBin(const char* FILE) : 
-    parent(nullptr)
-{
-    loadParamFile(FILE);
-}
+    m_parent(nullptr) { loadParamFile(FILE); }
 
 ParamBin::ParamBin(std::string fileName) : 
-    parent(nullptr)
-{
-    loadParamFile(fileName);
-}
-
-ParamBin::~ParamBin() 
-{ }
-
-ParamMap ParamBin::getParamMap() const {
-    return params;
-}
+    m_parent(nullptr) { loadParamFile(fileName); }
 
 ParamBin& ParamBin::operator<<(const NamedBin& named_bin)
 {
@@ -100,7 +92,7 @@ ParamBin& ParamBin::operator<<(const NamedBin& named_bin)
 std::string ParamBin::getStrParam(const std::string& name) const
 {
     std::string strval;
-    std::string key = pw::eatWhiteSpace(name);
+    std::string key = pwbin::eatWhiteSpace(name);
     if(searchParamMap(key,strval))
         return strval;
     if(rootAliasSearch(key,strval)) 
@@ -115,29 +107,29 @@ void ParamBin::set(const NamedBin& named_bin)
 
 void ParamBin::setAlias(const std::string& name,const std::string& alias)
 {
-    std::string key = pw::eatWhiteSpace(name);
-    if(params.count(key) > 0){
-        alias_map[alias] = key;
-        reverse_alias_map[key] = alias;
+    std::string key = pwbin::eatWhiteSpace(name);
+    if(m_params.count(key) > 0){
+        m_alias_map[alias] = key;
+        m_ralias_map[key] = alias;
     } else
         throw ParamBinKeyException(key);
 }
 
 std::string ParamBin::setParamKey(const std::string& name)
 {
-    std::string key = pw::eatWhiteSpace(pw::removeSubString(name,'(',')'));
-    std::string alias = pw::eatWhiteSpace(pw::subString(name,'(',')')); 
+    std::string key = pwbin::eatWhiteSpace(pwbin::removeSubString(name,'(',')'));
+    std::string alias = pwbin::eatWhiteSpace(pwbin::subString(name,'(',')')); 
     if(!alias.empty()){
-        alias_map[alias] = key;
-        reverse_alias_map[key] = alias;
+        m_alias_map[alias] = key;
+        m_ralias_map[key] = alias;
     }
     return key;
 }
 
 bool ParamBin::searchParamMap(const std::string& key,std::string& strval) const
 {
-    auto it = params.find(key);
-    if(it != params.cend()){
+    auto it = m_params.find(key);
+    if(it != m_params.cend()){
         strval = (*it).second;
         return true;
     }
@@ -147,13 +139,13 @@ bool ParamBin::searchParamMap(const std::string& key,std::string& strval) const
 bool ParamBin::searchAliasTree(const std::string& alias_key,std::string& strval) const
 {
     // Check current bin alias'
-    auto it = alias_map.find(alias_key);
-    if(it != alias_map.cend()){
+    auto it = m_alias_map.find(alias_key);
+    if(it != m_alias_map.cend()){
         std::string key = (*it).second;
         return searchParamMap(key,strval);
     }
     // Check child bins
-    for(const auto& kv : children) {
+    for(const auto& kv : m_children) {
         if(kv.second->searchAliasTree(alias_key,strval))
             return true;
     }
@@ -163,62 +155,55 @@ bool ParamBin::searchAliasTree(const std::string& alias_key,std::string& strval)
 bool ParamBin::rootAliasSearch(const std::string& alias_key,std::string& strval) const 
 {
     const ParamBin* top = this;
-    while(top->parent != nullptr)
-        top = top->parent;
+    while(top->m_parent != nullptr)
+        top = top->m_parent;
     return top->searchAliasTree(alias_key,strval);
 }
 
 
 template <>
-void convertFromString(const std::string& str,char& val)
-{
-    val = *str.c_str();
-}
+void convertFromString(const std::string& str,char& val) { val = *str.c_str(); }
 
 template <>
-void convertFromString(const std::string& str,std::string& val)
-{
-    val = str;
-}
+void convertFromString(const std::string& str,std::string& val) { val = str; }
 
 template <>
 void convertFromString(const std::string& str,std::vector<std::string>& vals)
 {
     vals.clear();
-    vals = pw::parseString(str,',');
+    vals = pwbin::parseString(str,',');
 }
 
 template <>
-std::string convertToString(std::string str)
-{
-    return str;
-}
+std::string convertToString(std::string str) { return str; }
 
 template <>
-std::string convertToString(const char* cstr)
-{
-    std::string str(cstr);
-    return str;
-}
+std::string convertToString(const char* cstr) { return std::string(cstr); }
+
+template <>
+std::string convertToString(char val) { return std::string(1,val); }
+
+
 
 void lineToNameVal(const std::string& line_feed,std::string& name,std::string& vals)
 {
-    std::vector<std::string> parsedParam = pw::parseString(line_feed,':');
+    std::vector<std::string> parsedParam = pwbin::parseString(line_feed,':');
     name = parsedParam[0];
     vals = parsedParam[1];
-    name = pw::eatWhiteSpace(name);
-    vals = pw::eatWhiteSpace(vals);
+    name = pwbin::eatWhiteSpace(name);
+    vals = pwbin::eatWhiteSpace(vals);
 }
 
 std::ifstream& readNextLine(std::ifstream& fin,std::string& line_feed) 
 {
     getline(fin,line_feed);
-    line_feed = pw::decommentString(line_feed,"#");
+    line_feed = pwbin::decommentString(line_feed,"#");
     // Check that line_feed is not whitespace
-    while(fin && pw::isWhitespace(line_feed)){
+    while(fin && pwbin::isWhitespace(line_feed)){
         getline(fin,line_feed);
-        line_feed = pw::decommentString(line_feed,"#");
+        line_feed = pwbin::decommentString(line_feed,"#");
     }
+
     return fin;
 }
 
@@ -235,21 +220,22 @@ void ParamBin::scanYAML(std::ifstream& fin)
 
     while(readNextLine(fin,line_feed))
     {
-				std::vector<std::string> parsed_line_feed = pw::parseString(line_feed,':');
+				std::vector<std::string> parsed_line_feed = pwbin::parseString(line_feed,':');
 				if(parsed_line_feed.size() == 0)
 						throw std::exception(); 
 				else if(parsed_line_feed.size() == 1){
 						throw std::exception(); 
 				} else if(parsed_line_feed.size() == 2){
-						std::string name(pw::eatWhiteSpace(parsed_line_feed[0]));
-						std::string val(pw::eatWhiteSpace(parsed_line_feed[1]));
+						std::string name(pwbin::eatWhiteSpace(parsed_line_feed[0]));
+						std::string val(pwbin::eatWhiteSpace(parsed_line_feed[1]));
 						// Found a group
 						if(val.empty()){
 								std::string group_name(parsed_line_feed[0]);
-								group_name = pw::eatWhiteSpace(group_name);
+								group_name = pwbin::eatWhiteSpace(group_name);
 								// Calculate amount of line whitespace to start string to
 								// determine parent/child relationship
-								level = pw::countFirstChar(line_feed," ");
+								level = pwbin::countFirstChar(line_feed," \t");
+//								std::cout << "GROUP: " << group_name << ", LEVEL: " << level << std::endl;
 								while(level <= levels.back() && parents.size() > 0){
 										parents.pop_back();
 										levels.pop_back();
@@ -257,7 +243,7 @@ void ParamBin::scanYAML(std::ifstream& fin)
 								levels.push_back(level);
 								ParamBin* bin = parents.back();
 								bin->setBin(group_name,std::unique_ptr<ParamBin>(new ParamBin));
-								parents.push_back(bin->children[group_name].get());
+								parents.push_back(bin->m_children[group_name].get());
 						}
 						else
                 parents.back()->set(NamedParam<std::string>(name,val));
@@ -293,17 +279,16 @@ void ParamBin::loadParamFile(std::string FILE)
 void ParamBin::printBin(std::ostream& os) const{
 
     static const std::string EMPTY_CHARS = std::string(2,' ');
-
     static int depth = 0;
     os << std::endl;
-    auto it = params.cbegin();
-    while(it != params.cend()){
-        std::string name;
+    auto it = m_params.cbegin();
+    while(it != m_params.cend()){
+        std::string name("");
         for(int i = 0; i < depth; i++)
             name += EMPTY_CHARS;
         name += it->first;
-        auto ait = reverse_alias_map.find(it->first);
-        if(ait != reverse_alias_map.cend())
+        auto ait = m_ralias_map.find(it->first);
+        if(ait != m_ralias_map.cend())
             name += '(' + ait->second + ')'; 
 
         os << std::setiosflags(std::ios::left) << std::setw(40) << name + ":";
@@ -311,10 +296,9 @@ void ParamBin::printBin(std::ostream& os) const{
         it++;
     }
     os << std::endl;
-    auto bit = children.cbegin();
-    while(bit != children.cend()){
-        std::string group_name;
-				// add empty chars before starting group name
+    auto bit = m_children.cbegin();
+    while(bit != m_children.cend()){
+        std::string group_name("");
         for(int i = 0; i < depth; i++)
             group_name += EMPTY_CHARS;
         group_name += bit->first;
@@ -335,28 +319,28 @@ std::ostream& operator<<(std::ostream& os,const ParamBin& bin)
 
 bool ParamBin::inBin(const std::string& name) const
 {
-    if(params.count(name) > 0 || children.count(name) > 0) 
+    if(m_params.count(name) > 0 || m_children.count(name) > 0) 
         return true;
     return false;
 }
 
 int ParamBin::size(const std::string& name) const
 {
-    auto it = params.find(name);
-    if(it != params.cend())
-        return pw::countCharacters(it->second,',') + 1;
+    auto it = m_params.find(name);
+    if(it != m_params.cend())
+        return pwbin::countCharacters(it->second,',') + 1;
     return 0;
 }
 
 bool ParamBin::empty() const
 {
-    return (params.empty() && children.empty() ? true : false);
+    return (m_params.empty() && m_children.empty() ? true : false);
 }
 
 ParamBin& ParamBin::getBin(const std::string& name) 
 {
-    auto it = children.find(name);
-    if(it != children.cend())
+    auto it = m_children.find(name);
+    if(it != m_children.cend())
         return *it->second;
     else
         throw ParamBinKeyException(name);
@@ -364,8 +348,8 @@ ParamBin& ParamBin::getBin(const std::string& name)
 
 const ParamBin& ParamBin::getBin(const std::string& name) const
 {
-    auto it = children.find(name);
-    if(it != children.cend())
+    auto it = m_children.find(name);
+    if(it != m_children.cend())
         return *it->second;
     else
         throw ParamBinKeyException(name);
@@ -375,8 +359,8 @@ const ParamBin& ParamBin::getBin(const std::string& name) const
 bool ParamBin::clear(const std::string& name)
 {
     if(inBin(name)){
-        params.erase(name);
-        children.erase(name);
+        m_params.erase(name);
+        m_children.erase(name);
         return true;
     }
     else 
@@ -385,83 +369,10 @@ bool ParamBin::clear(const std::string& name)
 
 void ParamBin::clear() 
 {
-    params.clear();
-    children.clear();
-    alias_map.clear();
-    reverse_alias_map.clear();
-}
-
-
-double ParamBin::getDbl(const std::string& name) const
-{
-    double val;
-    get(name,val);
-    return val;
-}
-
-int ParamBin::getInt(const std::string& nm) const
-{
-    int val;
-    get(nm,val);
-    return val;
-}
-
-std::string ParamBin::getStr(const std::string& nm) const
-{
-    std::string val;
-    get(nm,val);
-    return val;
-}
-
-std::string ParamBin::getStrL(const std::string& name) const
-{
-    return pw::stringLowerCase(getStr(name));
-}
-
-std::string ParamBin::getStrU(const std::string& name) const
-{
-    return pw::stringUpperCase(getStr(name));
-}
-
-char ParamBin::getChar(const std::string& name) const
-{
-    char val;
-    get(name,val);
-    return val;
-}
-
-bool ParamBin::getBool(const std::string& name) const
-{
-    std::string val;
-    get(name,val);
-    return (val == "on" ? true : false);
-}
-
-bool ParamBin::getBoolF(const std::string& name) const
-{ return (inBin(name) ? getBool(name) : false); }
-
-bool ParamBin::getBoolT(const std::string& name) const
-{ return (inBin(name) ? getBool(name) : true); }
-
-std::vector<double> ParamBin::getDblVec(const std::string& name) const
-{
-    std::vector<double> vals;
-    get(name,vals);
-    return vals;
-}
-
-std::vector<int> ParamBin::getIntVec(const std::string& name) const
-{
-    std::vector<int> vals;
-    get(name,vals);
-    return vals;
-}
-
-std::vector<std::string> ParamBin::getStrVec(const std::string& name) const
-{
-    std::vector<std::string> vals;
-    get(name,vals);
-    return vals;
+    m_params.clear();
+    m_children.clear();
+    m_alias_map.clear();
+    m_ralias_map.clear();
 }
 
 void ParamBin::setBool(const std::string& name,bool val){
@@ -477,15 +388,15 @@ void ParamBin::set(const std::string& name,const ParamBin& bin)
 void ParamBin::setBin(const std::string& name,const ParamBin& bin)
 {
     std::unique_ptr<ParamBin> child_bin(new ParamBin(bin));
-    child_bin->parent = this;
-    children[name] = std::move(child_bin);
+    child_bin->m_parent = this;
+    m_children[name] = std::move(child_bin);
 }
 
 // Pass by pointers simply transfers ownership of pointer
 void ParamBin::setBin(const std::string& name,std::unique_ptr<ParamBin> bin)
 {
-    bin->parent = this;
-    children[name] = std::move(bin);
+    bin->m_parent = this;
+    m_children[name] = std::move(bin);
 }
 
 std::vector<std::string> ParamBin::inBin(const std::vector<std::string>& nameVec) const
